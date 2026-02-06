713c29f73bb0af8a39fe2f5d6fff45efdf7a315f
Pierina Lopez <101863285+PierinaLopez@users.noreply.github.com>
2026-02-07 00:32:22 +0100

    Enable React renderer for admin components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   output: "server",
   adapter: netlify(),
+  integrations: [react()],
 });
